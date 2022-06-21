e1f7a53605ac18e9155c995d00a7ef715a5f5bd3
wangwenyuan <827287829@qq.com>
2022-06-21 15:09:49 +0800

    修改

--- src/index/Index/IndexController.java
+++ src/index/Index/IndexController.java
@@ -15,140 +15,37 @@ package index.Index;
 
 import java.io.UnsupportedEncodingException;
 import java.sql.SQLException;
-import java.util.HashMap;
-import java.util.Map;
-
-import com.javatodo.core.model.W;
-import com.javatodo.core.tools.Http;
-import com.javatodo.core.tools.T;
-
-import common.Common;
-import common.MU;
-import common.MobileWidget;
-import common.database.RENOVATION;
 
 public class IndexController extends CommonController {
 	public void indexPage() throws SQLException, UnsupportedEncodingException {
-		Map<String, W> where = new HashMap<String, W>();
-		where.put(RENOVATION.type, new W("eq", "Index/Index/index"));
-		where.put(RENOVATION.is_default, new W("eq", 1));
-		where.put(RENOVATION.is_del, new W("eq", 0));
-		Map<String, Object> map = new MU(RENOVATION._table_name).where(where).find();
-		if (map != null) {
-			String html = MobileWidget.buildPage(map.get(RENOVATION.id).toString(), webRequestParam);
-			this.htmlDisplay(html);
-		} else {
-			this.htmlDisplay(MobileWidget.noTemplateNotice());
-		}
+
 	}
 
 	public void customPage() throws SQLException, UnsupportedEncodingException {
-		Map<String, W> where = new HashMap<String, W>();
-		where.put(RENOVATION.type, new W("eq", "Index/Index/custom"));
-		where.put(RENOVATION.id, new W("eq", I(RENOVATION.id)));
-		where.put(RENOVATION.is_del, new W("eq", 0));
-		Map<String, Object> map = new MU(RENOVATION._table_name).where(where).find();
-		if (map != null) {
-			String html = MobileWidget.buildPage(map.get(RENOVATION.id).toString(), webRequestParam);
-			this.htmlDisplay(html);
-		} else {
-			this.htmlDisplay(MobileWidget.noTemplateNotice());
-		}
+
 	}
 
 	public void menuPage() throws SQLException, UnsupportedEncodingException {
-		Http http = new Http();
-		String category_sign = http.get(Common.unionHost + "/Public/category_sign.txt");
-		String html = T.S("Index/Index/menu" + category_sign);
-		if (html.equals("")) {
-			Map<String, W> where = new HashMap<String, W>();
-			where.put(RENOVATION.type, new W("eq", "Index/Index/menu"));
-			where.put(RENOVATION.is_default, new W("eq", 1));
-			where.put(RENOVATION.is_del, new W("eq", 0));
-			Map<String, Object> map = new MU(RENOVATION._table_name).where(where).find();
-			if (map != null) {
-				html = MobileWidget.buildPage(map.get(RENOVATION.id).toString(), webRequestParam);
-				T.S("Index/Index/menu" + category_sign, html);
-				String json = http.get(Common.unionHost + "/Public/category.json");
-				T.coverFile(servlet.getServletContext().getRealPath("/") + "json/category.json", json);
-				this.htmlDisplay(html);
-			} else {
-				this.htmlDisplay(MobileWidget.noTemplateNotice());
-			}
-		} else {
-			this.htmlDisplay(html);
-		}
+
 	}
 
 	public void categoryPage() throws SQLException, UnsupportedEncodingException {
-		Map<String, W> where = new HashMap<String, W>();
-		where.put(RENOVATION.type, new W("eq", "Index/Index/category"));
-		where.put(RENOVATION.is_default, new W("eq", 1));
-		where.put(RENOVATION.is_del, new W("eq", 0));
-		Map<String, Object> map = new MU(RENOVATION._table_name).where(where).find();
-		if (map != null) {
-			webRequestParam.put("category_id", I("id"));
-			String html = MobileWidget.buildPage(map.get(RENOVATION.id).toString(), webRequestParam);
-			this.htmlDisplay(html);
-		} else {
-			this.htmlDisplay(MobileWidget.noTemplateNotice());
-		}
+
 	}
 
 	public void categoryGoodsListPage() throws SQLException, UnsupportedEncodingException {
-		Map<String, W> where = new HashMap<String, W>();
-		where.put(RENOVATION.type, new W("eq", "Index/Index/category"));
-		where.put(RENOVATION.is_default, new W("eq", 1));
-		where.put(RENOVATION.is_del, new W("eq", 0));
-		Map<String, Object> map = new MU(RENOVATION._table_name).where(where).find();
-		if (map != null) {
-			webRequestParam.put("category_id", I("id"));
-			String html = MobileWidget.buildList(map.get(RENOVATION.id).toString(), webRequestParam);
-			this.htmlDisplay(html);
-		} else {
-			this.htmlDisplay(MobileWidget.noTemplateNotice());
-		}
+
 	}
 
 	public void searchPage() throws SQLException, UnsupportedEncodingException {
-		Map<String, W> where = new HashMap<String, W>();
-		where.put(RENOVATION.type, new W("eq", "Index/Index/search"));
-		where.put(RENOVATION.is_default, new W("eq", 1));
-		where.put(RENOVATION.is_del, new W("eq", 0));
-		Map<String, Object> map = new MU(RENOVATION._table_name).where(where).find();
-		if (map != null) {
-			String html = MobileWidget.buildPage(map.get(RENOVATION.id).toString(), webRequestParam);
-			this.htmlDisplay(html);
-		} else {
-			this.htmlDisplay(MobileWidget.noTemplateNotice());
-		}
+
 	}
 
 	public void searchGoodsListPage() throws SQLException, UnsupportedEncodingException {
-		Map<String, W> where = new HashMap<String, W>();
-		where.put(RENOVATION.type, new W("eq", "Index/Index/search"));
-		where.put(RENOVATION.is_default, new W("eq", 1));
-		where.put(RENOVATION.is_del, new W("eq", 0));
-		Map<String, Object> map = new MU(RENOVATION._table_name).where(where).find();
-		if (map != null) {
-			String html = MobileWidget.buildList(map.get(RENOVATION.id).toString(), webRequestParam);
-			this.htmlDisplay(html);
-		} else {
-			this.htmlDisplay(MobileWidget.noTemplateNotice());
-		}
+
 	}
 
 	public void detailPage() throws SQLException, UnsupportedEncodingException {
-		Map<String, W> where = new HashMap<String, W>();
-		where.put(RENOVATION.type, new W("eq", "Index/Index/goodsDetail"));
-		where.put(RENOVATION.is_default, new W("eq", 1));
-		where.put(RENOVATION.is_del, new W("eq", 0));
-		Map<String, Object> map = new MU(RENOVATION._table_name).where(where).find();
-		if (map != null) {
-			String html = MobileWidget.buildPage(map.get(RENOVATION.id).toString(), webRequestParam);
-			this.htmlDisplay(html);
-		} else {
-			this.htmlDisplay(MobileWidget.noTemplateNotice());
-		}
+
 	}
 }
